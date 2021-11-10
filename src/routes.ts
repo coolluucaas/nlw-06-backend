968d384b91fd0f2703b5b23783316ed4b112c81a
coolluucaas <lucaspsanta@gmail.com>
2021-11-10 18:19:04 -0300

    + CreateTag route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
+import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const router = Router()
 
 router.post('/users', new CreateUserController().handle)
+router.post('/tags', ensureAdmin, new CreateTagController().handle)
 
 export { router }
