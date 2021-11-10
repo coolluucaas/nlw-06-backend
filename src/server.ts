59482a10ef02e405303e464b572f3e7fb1fe1a7f
coolluucaas <lucaspsanta@gmail.com>
2021-11-10 13:15:35 -0300

    + server com middleware para tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,25 @@
-import express from 'express';
+import 'reflect-metadata'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
+import './database'
+import { router } from './routes'
 
-const app = express();
+const app = express()
 
-app.listen(3000, ()=> console.log('🛸 the server is running on port 3000');
+app.use(express.json())
+app.use(router)
+app.use(
+    (error: Error, request: Request, response: Response, next: NextFunction)=> {
+
+        if(error instanceof Error) {
+            return response.status(400).json({ error: error.message })
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error'
+        })
+    }
+)
+
+app.listen(3000, () => console.log('🛸 the server is running on port 3000'))
