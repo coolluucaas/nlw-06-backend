02f45e37a4a4145cd899b55fe88544b5c639cb28
coolluucaas <lucaspsanta@gmail.com>
2021-11-11 20:39:01 -0300

    + middleware: ensureAdmin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,15 +1,21 @@
 import { Request, Response, NextFunction } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { UserRepository } from '../repositories/UserRepository'
 
-export function ensureAdmin(
+export async function ensureAdmin(
     request: Request,
     response: Response,
     next: NextFunction
 ) {
-    const admin = false
-    if (admin) {
-        return next()
+    const userRepository = getCustomRepository(UserRepository)
+    const { user_id } = request
+
+    const { admin } = await userRepository.findOne({ id: user_id })
+
+    if (!admin) {
+        return response.status(401).json({
+            error: 'Unauthorized',
+        })
     }
-    return response.status(401).json({
-        error: "Unauthorized",
-    })
+    return next()
 }
