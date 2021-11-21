54ed85b3c41572adcea9a2cc63a4662754ae29ee
coolluucaas <lucaspsanta@gmail.com>
2021-11-21 15:31:57 -0300

    +endpoints routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express'
+import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { CreateComplimentController } from './controllers/CreateComplimentController'
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
+import { ListComplimentsUserReceivedController } from './controllers/ListComplimentsUserReceivedController'
+import { ListComplimentsUserSentController } from './controllers/ListComplimentsUserSentController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated'
 
 const router = Router()
 
 router.post('/users', new CreateUserController().handle)
-router.post('/tags', ensureAdmin, new CreateTagController().handle)
+router.post('/tags', new CreateTagController().handle)
+router.post('/authenticate', new AuthenticateUserController().handle)
+router.post(
+    '/compliments',
+    ensureAuthenticated,
+    ensureAdmin,
+    new CreateComplimentController().handle
+)
+router.get('users/compliments/received',ensureAuthenticated, new ListComplimentsUserReceivedController().handle)
+router.get('users/compliments/sent',ensureAuthenticated, new ListComplimentsUserSentController().handle)
 
 export { router }
